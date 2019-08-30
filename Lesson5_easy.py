fc6424ee5fd82f8f7eb4678fb290dc305daf7bd7
Shaman2019 <sharapov_am@mail.ru>
2019-08-30 14:49:13 +0300

    Lesson 4 easy + normal

--- Lesson5_easy.py
+++ Lesson5_easy.py
@@ -2,26 +2,28 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
-
 import os
+
 dirnames=['dir_'+str(d) for d in range(1,10)]
-def make9dirs(names):
-    for dirn in names:
-        os.mkdir(os.path.join(os.getcwd(),dirn))
+def make_dir(name):
+    os.mkdir(os.path.join(os.getcwd(),name))
 
-def rm9dirs(names):
-    for dirn in names:
-        os.rmdir(os.path.join(os.getcwd(),dirn))
+def rm_dir(name):
+    os.rmdir(os.path.join(os.getcwd(),name))
+def start_lesson():
+    for dir1 in dirnames:
+        make_dir(dir1)
 
+    for dir2 in dirnames:
+        rm_dir(dir2)
+start_lesson()
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-
-with os.scandir(".") as it:
-    for entry in it:
-        if entry.is_dir():
-            print(entry.name)
-
-
+def printdirs():
+    with os.scandir(".") as it:
+        for entry in it:
+            if entry.is_dir():
+                print(entry.name)
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 import shutil
